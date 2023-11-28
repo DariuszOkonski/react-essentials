5cca84d249ddfdd3f114c0501ca5d9b77312d55a
Dariusz Okoński <darek200180@gmail.com>
2023-11-28 21:39:56 +0100

    final

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 
-export default function Tabs({ children, buttons, ButtonsContainer }) {
+export default function Tabs({ children, buttons, ButtonsContainer = 'menu' }) {
   // const ButtonsContainer = buttonsContainer;
 
   return (
